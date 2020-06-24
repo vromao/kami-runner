aaa0f10851547daed1e87061ae50ebb4c8b34a82
Vinícius Romão <viniciusromao.contato@gmail.com>
2020-06-23 23:49:54 -0300

    Clean initial webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,8 @@
-const webpack = require('webpack');
 const path = require('path');
-
-// const miniCssExtractPlugin = require('mini-css-extract-plugin');
-// const optimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin'); // esse plugin é para ser usando em ambiente de produção. Na hora de buildar, não para dev.
-
 const htmlWebpackPlugin = require('html-webpack-plugin');
-// const copyWebpackPlugin = require('copy-webpack-plugin');
-
 const devMode = process.env.NODE_ENV || 'production';
-
 const plugins = [];
 
-// plugin para tratar o html. Importar os arquivos automaticamente, minificar e versionar arquivos importados.
 plugins.push(new htmlWebpackPlugin({
     hash: true,
     minify: {
@@ -23,30 +14,9 @@ plugins.push(new htmlWebpackPlugin({
     template:  path.resolve(__dirname, 'src', 'index.html'),
 }));
 
-// plugins.push(new copyWebpackPlugin(
-
-// ));
-
-
-if(!devMode) {
-    plugins.push(new webpack.optimize.ModuleConcatenationPlugin()); //tratamento para evitar que o webpack coloque cada modulo em uma closure durante a criação desses modulos. Isso acelera o tipo de processamento e carregamento no navegador para a build produção. Todos os modulos são concatenados em um unico wrapper.
-    plugins.push(new babiliPlugin());
-
-    plugins.push(new optimizeCSSAssetsPlugin({
-        cssProcessor: require('cssnano'),
-        cssProcessorOptions: {
-            discardComments: {
-                removeAll: true
-            }
-        },
-        canPrint: true
-    }));
-}
-
 module.exports = {
     entry: {
         app: './src/index.js',
-        // vendor: ['jquery','bootstrap','reflect-metadata'] // esse atributo deve ter o MESMO nome do name do CommonsChunkPlugin
     },
     output: {
         filename: 'bundle.[hash:4].js',
